ba05ebb9c23c4bad3c4d46581c4a3ec18e0e7c7d
Adrià Muñoz <adria.munoz@enti.cat>
2022-04-09 17:17:40 +0200

    V1.9 Cromatic Effect

--- Assets/Scripts/CustomPostpro.cs
+++ Assets/Scripts/CustomPostpro.cs
@@ -9,13 +9,14 @@ using UnityEngine.Rendering.PostProcessing;
 //Using [PostProcess()] attrib allows us to tell Unity that the class holds postproccessing data. 
 [PostProcess(renderer: typeof(CustomPostpro),//First parameter links settings with actual renderer
             PostProcessEvent.AfterStack,//Tells Unity when to execute this postpro in the stack
-            "Custom/InvertColors")] //Creates a menu entry for the effect
+            "Unlit/Blood")] //Creates a menu entry for the effect
                                     //Forth parameter that allows to decide if the effect should be shown in scene view
 public sealed class CustomPostproSettings : PostProcessEffectSettings
 {
     [Range(0f, 1f), Tooltip("Effect Intensity.")]
-    public FloatParameter blend = new FloatParameter { value = 0.0f }; //Custom parameter class, full list at: /PostProcessing/Runtime/
-                                                                        //The default value is important, since is the one that will be used for blending if only 1 of volume has this effect
+    public FloatParameter blend = new FloatParameter { value = 0.5f }; //Custom parameter class, full list at: /PostProcessing/Runtime/
+    public FloatParameter intensity = new FloatParameter { value = 0.5f };
+    public ColorParameter effectColor = new ColorParameter { value = Color.white };                                                                    //The default value is important, since is the one that will be used for blending if only 1 of volume has this effect
 }
 
 public class CustomPostpro : PostProcessEffectRenderer<CustomPostproSettings>//<T> is the setting type
@@ -23,9 +24,11 @@ public class CustomPostpro : PostProcessEffectRenderer<CustomPostproSettings>//<
     public override void Render(PostProcessRenderContext context)
     {
         //We get the actual shader property sheet
-        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Custom/InvertColors"));
+        var sheet = context.propertySheets.Get(Shader.Find("Hidden/Unlit/Blood"));
         //Set the uniform value for our shader
-        sheet.properties.SetFloat("_intensity", settings.blend);
+        sheet.properties.SetFloat("_Blend", settings.blend);
+        sheet.properties.SetColor("_Intensity", Color.HSVToRGB(0, 0, settings.intensity));
+        sheet.properties.SetColor("_EffectColor", settings.effectColor);
 
         //We render the scene as a full screen triangle applying the specified shader
         context.command.BlitFullscreenTriangle(context.source, context.destination, sheet, 0);
